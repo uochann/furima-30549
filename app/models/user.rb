838e1b1b15ee4aa6b78f8f8c1335b5f382fc9dd4
uochann <ma602sa@gmail.com>
2020-10-09 14:26:24 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
          
+
+
+         validates :nickname, presence: true
+         validates :birthday, presence: true
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/+\z }
+         validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/+\z }
+         validates :last_name_kana, presence: true, format: { with: /\p{katakana}/ }
+         validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
 end
