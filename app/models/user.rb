8dccda629d0c639b8127fc94f245e26728dde358
uochann <ma602sa@gmail.com>
2020-10-12 12:18:13 +0900

    ルボコップ実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :items
+  has_many :items
 
-        with_options presence: true do
-          validates :nickname
-          validates :birthday
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
 
-          with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
-            validates :last_name
-            validates :first_name
-          end
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
 
-          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-            validates :last_name_kana
-            validates :first_name_kana
-          end
-        end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
