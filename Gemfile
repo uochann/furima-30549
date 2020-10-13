7fabd38df44c5ca46d1d9994b6067b5b11bff9b5
uochann <ma602sa@gmail.com>
2020-10-13 15:41:54 +0900

    Active Storage 導入

--- Gemfile
+++ Gemfile
@@ -66,3 +66,6 @@ gem 'devise'
 gem 'pry-rails'
 
 gem 'rails-i18n'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    unless @item.valid?
+      render :new and return
+    end
+
+    @item.save
+    redirect_to root_path
+  end
+  
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :prefecture_id, :condition_id, :category_id, :postage_type_id, :preparation_day_id, :image).merge(user_id: current_user.id)
+  end
+
 end
