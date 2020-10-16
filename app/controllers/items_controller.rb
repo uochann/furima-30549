6fe194f96875d6946e5ff69c666daac1acb4134b
uochann <ma602sa@gmail.com>
2020-10-16 11:28:08 +0900

    外部スクリプトの埋め込み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
